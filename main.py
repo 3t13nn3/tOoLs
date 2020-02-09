71076e0b500da2f4160728187ddeeba573b88b94
3t13nn3 <etiennepenault1997@gmail.com>
2020-02-09 20:28:51 +0100

    Init

---
+++ main.py
@@ -0,0 +1,43 @@
+#TO ADD: Copy to clipboard thing avoiding pyperclip
+
+import sys
+
+def data_recovery():
+	text = []
+	words = []
+	if sys.argv[1] == "1":
+		while True:
+			try:
+				line = input()
+			except EOFError:
+				break
+			words.append(line + "\n")
+		words[len(words)-1] = words[len(words)-1][:len(words[len(words)-1])-1]
+	else:
+		words = sys.argv[2:]
+		for i in  range(len(words)):
+			words[i] += " "
+
+	for i in words:
+		text.append(str(i))
+
+	return text
+
+
+def tRaNsFoRm(og_text):
+	text = list("".join(og_text))
+	for i in range(len(text)):
+		if i%2 == 1:
+			text[i] = text[i].upper()
+		else:
+			text[i] = text[i].lower()
+
+	return "".join(text)
+
+
+if __name__ == "__main__":
+	
+	og_text = data_recovery()
+	text    = tRaNsFoRm(og_text)
+
+	print(text)
